2f9b6fb3f55c6b627b08612ca610da26e671b6c7
temporalcs <zhouyucs@icloud.com>
2024-08-07 18:34:01 +0800

    Update exp_cifar10_cnn.py

--- exps/exp_cifar10_cnn.py
+++ exps/exp_cifar10_cnn.py
@@ -8,11 +8,11 @@ class NewConf(object):
         experiment=["debug"],
         # use world to control the distribution of clients on cuda devices.
         # for advanced usage, use world_conf instead, see ./pcode/utils/topology.py
-        world=["0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1"],
+        world=["0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2"],
         on_cuda=[True],
         python_path=["/opt/conda/bin/python"],
         hostfile=["env/hostfile"],
-        manual_seed=[7],
+        manual_seed=[666],
         same_seed_process=[False],
         # general for the training.
         track_time=[True],
@@ -20,11 +20,11 @@ class NewConf(object):
         # general for fl.
         n_clients=[20],
         data=["cifar10"],
-        data_dir=["~/data"],
+        data_dir=["/home/default_user/data"],
         batch_size=[32],
         num_workers=[0],
         # fl master
-        n_comm_rounds=[100],
+        n_comm_rounds=[130],
         early_stopping_rounds=[0],
         # fl clients
         arch=["simple_cnn"],
@@ -37,21 +37,29 @@ class NewConf(object):
         lr=[0.01],
         personal_lr=[0.01],
         participation_ratio=[1.0],
-        partition_data_conf=["distribution=non_iid_dirichlet,non_iid_alpha=0.1,size_conf=1:1",
-                             "distribution=non_iid_dirichlet,non_iid_alpha=1.0,size_conf=1:1"],
-        personalization_scheme=["method=Normal",
-                                "method=Fine_tune",
-                                "method=FedRep",
-                                "method=Ditto",
-                                "method=GMA",
-                                "method=FedRod",
-                                "method=Memo_global",
-                                "method=Memo_personal",
-                                "method=THE",
-                                "method=THE_FT",
-                                "method=T3A",
-                                "method=ttt",
-                                "method=tsa",
-                                "method=knn_per",
+        q_level=[2],
+        sigma=[0.001],
+        mu=[0.7],
+        nu=[0],
+        cof=[5],
+        temperature=[0.05],
+        kb=[4],
+        talpha=[0.5],
+        partition_data_conf=[                             
+                                 "distribution=non_iid_dirichlet,non_iid_alpha=0.7,size_conf=1:1",              
+                                   
+                             ],
+        personalization_scheme=[                              
+                              
+                                             
+                              
+                            
+                               "method=BTFL", 
+                                  
+                              
+                            
+                              
+                                
+                               
                                 ],
     )
